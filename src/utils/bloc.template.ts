73926b2336c05c37631e265681fd4c8531e3ddc2
carrizomiguel <carrizo2000miguel@gmail.com>
2023-11-26 13:24:30 -0500

    fix: :heavy_plus_sign: import equatable on bloc template

--- src/utils/bloc.template.ts
+++ src/utils/bloc.template.ts
@@ -5,13 +5,14 @@ export function getBlocTemplate(blocName: string) {
   const snakeCaseBlocName = changeCase.snakeCase(blocName);
   const blocState = `${pascalCaseBlocName}State`;
   const blocEvent = `${pascalCaseBlocName}Event`;
-  return `import 'package:toke_bloc/toke_bloc.dart';
+  return `import 'package:equatable/equatable.dart';
+import 'package:toke_bloc/toke_bloc.dart';
 
 part '${snakeCaseBlocName}_event.dart';
 part '${snakeCaseBlocName}_state.dart';
 
 class ${pascalCaseBlocName}Bloc extends Bloc<${blocEvent}, ${blocState}> {
-  ${pascalCaseBlocName}Bloc() : super(${pascalCaseBlocName}.initial()) {
+  ${pascalCaseBlocName}Bloc() : super(${pascalCaseBlocName}State.initial()) {
     on<${blocEvent}>((event, emit) {
       // TODO: implement event handler
     });
